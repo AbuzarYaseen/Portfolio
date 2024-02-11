9d638172c07abdcf6951b4fac62fbd1ad317ece3
Abuzar Yaseen <abuzaryaseen88@gmail.com>
2024-02-12 00:19:27 +0500

    improvements made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,43 +2,71 @@ import React from "react";
 import { FaPhone, FaLinkedin, FaHome, FaInfo, FaGithub } from "react-icons/fa";
 import { GiSkills } from "react-icons/gi";
 
-import { Link, NavLink } from "react-router-dom";
-
 const Navbar = () => {
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const linkedInLink = "https://www.linkedin.com/in/abuzar-yaseen/";
+
   return (
     <>
-      <section className="md:flex md:justify-evenly lg:py-5 py-5  shadow-lg shadow-blue-600/50 md:shadow-xl md:shadow-blue-500/50">
-        <div className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0 ">
-          <FaHome size="1.5em " />
-          <a className="text-center  font-bold px-4 lg:text-lg text-sm ">
-            Home
-          </a>
-        </div>
-        <div className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0 ">
-          <FaInfo size="1.3em " />
-          <a className="text-center lg:px-2  font-bold px-4 lg:text-lg text-sm">
-            About
-          </a>
-        </div>
-        <div className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0 ">
-          <GiSkills size="1.5em " />
-          <a className="text-center lg:px-2  font-bold px-4 lg:text-lg text-sm">
-            Skills
-          </a>
-        </div>
-        <div className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0 ">
-          <FaLinkedin size="1.4em " />
-          <a className="text-center lg:px-2  font-bold px-4 lg:text-lg text-sm">
-            Connect
-          </a>
-        </div>
-        <div className="flex hover:animate-pulse  animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0 ">
-          <FaPhone size="1.2em " />
-          <a className="text-center lg:px-2  font-bold px-4 lg:text-lg text-sm">
-            Contact
-          </a>
-        </div>
-      </section>
+      <nav className="sticky top-0 bg-transparent z-10 backdrop-blur">
+        <ul className="md:flex md:justify-evenly lg:py-5 py-5 shadow-lg shadow-blue-600/50 md:shadow-xl md:shadow-blue-500/50">
+          <li
+            className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0"
+            onClick={() => scrollTo("home")}
+          >
+            <FaHome size="1.5em" />
+            <span className="text-center font-bold px-4 lg:text-lg text-sm">
+              Home
+            </span>
+          </li>
+
+          <li
+            className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0"
+            onClick={() => scrollTo("about")}
+          >
+            <FaInfo size="1.3em" />
+            <span className="text-center lg:px-2 font-bold px-4 lg:text-lg text-sm">
+              About
+            </span>
+          </li>
+
+          <li
+            className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0"
+            onClick={() => scrollTo("skills")}
+          >
+            <GiSkills size="1.5em" />
+            <span className="text-center lg:px-2 font-bold px-4 lg:text-lg text-sm">
+              Skills
+            </span>
+          </li>
+
+          <li
+            className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0"
+            onClick={() => scrollTo("contact")}
+          >
+            <FaPhone size="1.2em" />
+            <span className="text-center lg:px-2 font-bold px-4 lg:text-lg text-sm">
+              Contact
+            </span>
+          </li>
+
+          <li
+            className="flex hover:animate-pulse animate__animated animate__flip cursor-pointer px-6 lg:px-0 py-2 lg:py-0"
+            onClick={() => window.open(linkedInLink, "_blank")}
+          >
+            <FaLinkedin size="1.4em" />
+            <span className="text-center lg:px-2 font-bold px-4 lg:text-lg text-sm">
+              Connect
+            </span>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
